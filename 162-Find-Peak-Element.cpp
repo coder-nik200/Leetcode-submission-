d94a32228b4856fd0cf8ea973901bb946033f15d
Coder_nik01 <nitishkb1707@gmail.com>
2026-09-04 01:34:47 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 48.77%] - LeetPush

---
+++ 162-Find-Peak-Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int st = 0;
+        int end = nums.size() - 1;
+
+        while (st < end) {
+            int mid = st + (end - st) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                st = mid + 1;
+            }
+        }
+
+        return st;
+    }
+};
